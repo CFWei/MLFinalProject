d95da92d271c1029cef6fc6a8e108fd1152eef69
CFWei <s124210@gmail.com>
2014-12-22 14:17:49 +0800

    first commit

---
+++ GetStock.py
@@ -0,0 +1,125 @@
+__author__ = 'CFWei'
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+import csv
+
+from datetime import datetime
+
+filePath="stock/";
+
+def getStock(date,stockNum):
+    try:
+        searchDateTime = datetime.strptime(date,"%Y/%m/%d")
+        endTime_y = searchDateTime.strftime("%Y")
+        endTime_m = str(int(searchDateTime.strftime("%m"))-1)
+        endTime_d = searchDateTime.strftime("%d")
+
+        startTime_y = str(2010)
+        startTime_m = str(1-1)
+        startTime_d = str(1)
+
+        #Get Stock File
+        url= "http://real-chart.finance.yahoo.com/table.csv?s="+str(stockNum)+".TW&d="+endTime_m+"&e="+endTime_d+"&f="+endTime_y+"&g=d&a="+startTime_m+"&b="+startTime_d+"&c="+startTime_y+"&ignore=.csv"
+        response = urllib.request.urlopen(url)
+        csv_result = response.read().decode('utf-8')
+
+        #Save File
+        if not os.path.exists(filePath):
+            os.mkdir(filePath)
+        file = open(filePath+str(stockNum), 'w', encoding='utf-8')
+        file.write(csv_result)
+        file.close()
+    except:
+        print(str(stockNum)+" Get Stock Error")
+
+def getDJI(date):
+    try:
+        searchDateTime = datetime.strptime(date,"%Y/%m/%d")
+        endTime_y = searchDateTime.strftime("%Y")
+        endTime_m = searchDateTime.strftime("%b")
+        endTime_d = searchDateTime.strftime("%d")
+
+        startTime_y = str(2010)
+        startTime_m = "Jan"
+        startTime_d = str(1)
+
+        url = "http://www.google.com/finance/historical?q=INDEXDJX%3A.DJI&ei=oWwmVKCtJYSTlAX03oGICg&startdate=" + startTime_m + "+" + startTime_d + "+" + startTime_y + "&enddate=" + endTime_m + "+" + endTime_d + "+" + endTime_y
+
+        #Get Response From URL
+        response=urllib.request.urlopen(url)
+        #Get Byte Array From URL Content
+        html = response.read()
+        #Convert Byte Array to String
+        shtml = html.decode('utf-8')
+
+        #Create BeautifulSoup
+        soup = BeautifulSoup(shtml, 'lxml')
+
+        priceTable = soup.find_all(id='prices')
+
+        #Get DJI Title
+        DJITitle=[]
+        for title in priceTable[0].find_all('th' , class_ = 'bb'):
+            DJITitle.append(title.contents[0].strip('\n'))
+
+        #Get DJI List
+        DJIList = []
+        for child in priceTable[0].find_all('tr'):
+            temp = []
+            for child1 in child.find_all('td'):
+                temp.append(child1.contents[0].strip(',').strip('\n').replace(',', ''))
+
+            if temp != []:
+                DJIList.append(temp)
+        #Save to file
+        file = open(filePath+'DJI', 'w', encoding='utf-8')
+        flag = 0
+        for title in DJITitle:
+            if flag !=0:
+                file.write(',')
+            else:
+                flag=flag+1
+            file.write(title)
+        file.write('\n')
+        for items in DJIList:
+            flag = 0
+            for item in items:
+                if flag == 0:
+                    date = item.split(' ')
+                    month = datetime.strptime(date[0],"%b").strftime('%m')
+                    if len(date[1]) == 1:
+                        date[1] = '0' + date[1]
+                    day =  datetime.strptime(date[1],"%d").strftime('%d')
+                    year = date[2]
+                    file.write(year+'-'+month+'-'+day)
+                    flag = 1
+                else:
+                    file.write(',')
+                    file.write(item)
+
+            file.write('\n')
+        file.close()
+
+    except:
+        print('Get DJI Fail')
+
+def parseStock(date,stockNum,type):
+    try:
+        searchDateTime = datetime.strptime(date,"%Y/%m/%d").strftime("%Y-%m-%d")
+        with open(filePath+str(stockNum)) as f:
+            reader = csv.reader(f)
+            isTitleFlag = 0
+            for row in reader:
+                if isTitleFlag == 0:
+                    isTitleFlag = 1
+                else:
+                    #(row[0])
+                    if searchDateTime == row[0]:
+                        return row[type]
+
+        return 0
+
+    except:
+        print("Date:"+str(date) + " Type:" + str(type)  + " StockNum:" +str(stockNum)+" Parse Stock Error")
+        return 0
---
+++ open_price.py
@@ -0,0 +1,56 @@
+__author__ = 'CFWei'
+import talib
+import time
+import datetime
+from numpy import *
+
+from GetStock import getStock,parseStock,getDJI
+
+def OpenPrice(date,stockNum):
+    return parseStock(date,stockNum,1)
+
+def HighPrice(date,stockNum):
+    return parseStock(date,stockNum,2)
+
+def LowPrice(date,stockNum):
+    return parseStock(date,stockNum,3)
+
+def ClosePrice(date,stockNum):
+    return parseStock(date,stockNum,4)
+
+def Volume(date,stockNum):
+    return parseStock(date,stockNum,5)
+
+def DJI(date):
+    return parseStock(date,"DJI",4)
+
+def MACD(date,stocknum):
+    optInFastPeriod = 28
+    optInSlowPeriod = 12
+    optInSignalPeriod = 9
+    DataCount= optInFastPeriod + optInSignalPeriod - 1
+    searchDateTime = datetime.datetime.strptime(date,"%Y/%m/%d")
+    #Record How many days are used
+    dateCount=0
+    #Record How many data are ured
+    count = 0
+    closePrice = []
+    while(1):
+        tmpDate = searchDateTime - datetime.timedelta(days=dateCount)
+        tmpClosePrice = ClosePrice(tmpDate.strftime("%Y/%m/%d"),stocknum)
+        if tmpClosePrice != 0:
+            count = count + 1
+            closePrice.append(float(tmpClosePrice))
+        dateCount = dateCount +1
+        if count >= DataCount:
+            break
+    closePrice.reverse()
+    outMACD ,outMACDSignal ,outMACDHist=talib.MACD(array(closePrice),optInFastPeriod,optInSlowPeriod,optInSignalPeriod)
+    print(outMACD[len(outMACD)-1])
+    return 0
+
+
+MACD('2014/12/01',3705)
+
+#outMACD ,outMACDSignal ,outMACDHist=talib.MACD(close,12,26,9)
+#print(outMACD)
